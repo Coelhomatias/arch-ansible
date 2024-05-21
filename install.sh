c905969e927a71dad2c1292a5ccca35c03aa9f94
Coelhomatias <lenadrohenrique.lf@gmail.com>
2024-05-21 21:38:02 +0100

    chore: Add reboot task to Ansible playbook

---
+++ install.sh
@@ -0,0 +1,7 @@
+#/usr/bin/bash
+
+echo "Installing Ansible collections from requirements.yml..."
+ansible-galaxy collection install -r ~/arch-ansible/requirements.yml
+
+echo "Running the ansible playbook. Please enter your sudo password when prompted."
+ansible-playbook ~/arch-ansible/playbooks/common.yml -K
